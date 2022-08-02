8e52c8f35c8199849559968cb6ac0260c61037da
tungdn94 <tung.daongoc94@gmail.com>
2022-08-02 21:30:39 +0700

    Feature: adding MySQL-Docker and docker-compose.yml

---
+++ mysql-docker/Books.sql
@@ -0,0 +1,25 @@
+create table Books (
+	id INT,
+	Title VARCHAR(50),
+	Author VARCHAR(50)
+);
+insert into Books (id, Title, Author) values (1, 'Horizontal explicit service-desk', 'Aleda Wadeson');
+insert into Books (id, Title, Author) values (2, 'Networked incremental task-force', 'Sherill Scheu');
+insert into Books (id, Title, Author) values (3, 'Up-sized secondary synergy', 'Matty Balston');
+insert into Books (id, Title, Author) values (4, 'Grass-roots heuristic framework', 'Arlie Walkden');
+insert into Books (id, Title, Author) values (5, 'Extended composite productivity', 'Sandi Cudde');
+insert into Books (id, Title, Author) values (6, 'Horizontal heuristic algorithm', 'Mireielle Decreuze');
+insert into Books (id, Title, Author) values (7, 'Multi-tiered radical ability', 'Ardelia Pocock');
+insert into Books (id, Title, Author) values (8, 'Persistent explicit capacity', 'Zea Sprules');
+insert into Books (id, Title, Author) values (9, 'Seamless tertiary time-frame', 'Robbie Malcolm');
+insert into Books (id, Title, Author) values (10, 'User-friendly non-volatile info-mediaries', 'Cully Dhillon');
+insert into Books (id, Title, Author) values (11, 'Face to face homogeneous paradigm', 'Wilbur Speller');
+insert into Books (id, Title, Author) values (12, 'Profit-focused intermediate time-frame', 'Windy Slaight');
+insert into Books (id, Title, Author) values (13, 'Adaptive motivating paradigm', 'Dulcia Izacenko');
+insert into Books (id, Title, Author) values (14, 'Integrated zero defect challenge', 'Querida Gooble');
+insert into Books (id, Title, Author) values (15, 'Future-proofed well-modulated flexibility', 'Napoleon Rustich');
+insert into Books (id, Title, Author) values (16, 'Pre-emptive client-server website', 'Der Brister');
+insert into Books (id, Title, Author) values (17, 'Networked uniform service-desk', 'Collie Arzu');
+insert into Books (id, Title, Author) values (18, 'Progressive user-facing toolset', 'Gunther Drover');
+insert into Books (id, Title, Author) values (19, 'Extended multi-tasking installation', 'Lenee Allery');
+insert into Books (id, Title, Author) values (20, 'Decentralized logistical contingency', 'Shauna Di Napoli');
